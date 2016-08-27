985e9df804b351e64848ce4b19679f60ad636158
Raphael Deem <raphael.deem@gmail.com>
2016-08-27 16:34:49 -0700

    refactor, allows multiple methods per resource

--- thunder.py
+++ thunder.py
@@ -1,24 +1,39 @@
 import tornado.ioloop
 import tornado.web
 import inspect
+from collections import defaultdict
 
+app = []
 env = tornado.web.Application()
 
-def get_handler(function):
+def handler(methods):
     '''
     Returns a tornado handler with get method defined by the function passed
     via the get decorator
     '''
     class Handler(tornado.web.RequestHandler):
-        def get(self):
-            if len(inspect.getargspec(function).args) == 0:
-                self.write(function())
-            elif len(inspect.getargspec(function).args) == 1:
-                self.write(function(self.request))
+        if 'get' in methods.keys():
+            def get(self):
+                if len(inspect.getargspec(methods['get']).args) == 0:
+                    self.write(methods['get'])
+                elif len(inspect.getargspec(methods['get']).args) == 1:
+                    self.write(methods['get'](self.request))
+        if 'post' in methods.keys():
+            def post(self):
+                if len(inspect.getargspec(methods['post']).args) == 0:
+                    self.write(methods['post'])
+                elif len(inspect.getargspec(methods['post']).args) == 1:
+                    self.write(methods['post'](self.request))
+        if 'put' in methods.keys():
+            def put(self):
+                if len(inspect.getargspec(methods['put']).args) == 0:
+                    self.write(methods['put'])
+                elif len(inspect.getargspec(methods['put']).args) == 1:
+                    self.write(methods['put'](self.request))
     return Handler
 
 
-# should take optional arguments for path
+# Decorators
 def get(path='/'):
     '''
     Adds a function and optional path to global env instance
@@ -27,47 +42,34 @@ def get(path='/'):
     def _get(func_to_decorate):
         def new_func(*original_args, **original_kwargs):
             return func_to_decorate(*original_args, **original_kwargs)
-        #if env.
-        env.add_handlers(r".*", [(path, get_handler(func_to_decorate))])
+        #env.add_handlers(r".*", [(path, get_handler(func_to_decorate))])
+        app.append([r".*", [path, 'get', func_to_decorate]])
     return _get
 
-def put_handler(function):
-    class Handler(tornado.web.RequestHandler):
-        def put(self):
-            if len(inspect.putargspec(function).args) == 0:
-                self.write(function())
-            elif len(inspect.putargspec(function).args) == 1:
-                self.write(function(self.request))
-    return Handler
-
-
-# should take optional arguments for path
-def put(path='/'):
-    def _put(func_to_decorate):
-        def new_func(*original_args, **original_kwargs):
-            return func_to_decorate(*original_args, **original_kwargs)
-        env.add_handlers(r".*", [(path, put_handler(func_to_decorate))])
-    return _put
-
-
-
-def post_handler(function):
-    class Handler(tornado.web.RequestHandler):
-        def post(self):
-            if len(inspect.postargspec(function).args) == 0:
-                self.write(function())
-            elif len(inspect.postargspec(function).args) == 1:
-                self.write(function(self.request))
-    return Handler
-
-
-# should take optional arguments for path
 def post(path='/'):
+    '''
+    Adds a function and optional path to global env instance
+    '''
+    # this should match against existing env and append to existing ones
     def _post(func_to_decorate):
         def new_func(*original_args, **original_kwargs):
             return func_to_decorate(*original_args, **original_kwargs)
-        env.add_handlers(r".*", [(path, post_handler(func_to_decorate))])
+        #env.add_handlers(r".*", [(path, get_handler(func_to_decorate))])
+        app.append([r".*", [path, 'post', func_to_decorate]])
     return _post
+
+def put(path='/'):
+    '''
+    Adds a function and optional path to global env instance
+    '''
+    # this should match against existing env and append to existing ones
+    def _put(func_to_decorate):
+        def new_func(*original_args, **original_kwargs):
+            return func_to_decorate(*original_args, **original_kwargs)
+        #env.add_handlers(r".*", [(path, get_handler(func_to_decorate))])
+        app.append([r".*", [path, 'put', func_to_decorate]])
+    return _put
+
 
 # move to test
 @post('/test')
@@ -88,7 +90,15 @@ def headers(request):
     return str(request.body)
 
 def start(port=8888):
-    import pdb; pdb.set_trace()
+    apps = defaultdict(dict)
+    for key, value in app:
+        if value[0] in apps[key].keys():
+            apps[key][value[0]].append(value[1:])
+        else:
+            apps[key][value[0]] = [value[1:]]
+    for key in apps.keys():
+        for path in apps[key]:
+            env.add_handlers(key, [(path, handler(dict(apps[key][path])))])
     env.listen(port)
     tornado.ioloop.IOLoop.current().start()
 
